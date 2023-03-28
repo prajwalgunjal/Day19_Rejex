c4725cf509b783e13a25ec47a4c1045d45b1fd39
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-03-28 12:15:51 +0530

    UC2:added

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -1,21 +1,20 @@
 package com.bridgelabz;
-
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 public class UserRegistrationProblem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Fist name:- ");
         String name = sc.next();
+        System.out.print("Enter Last name:- ");
+        String lname = sc.next();
         Pattern pattern = Pattern.compile("^[A-Z]?[a-z]{3,}$");
         Matcher matcher = pattern.matcher(name);
-        if (matcher.matches())
+        Matcher matcher1 = pattern.matcher(lname);
+        if (matcher.matches()&& matcher1.matches())
             System.out.println("valid");
         else
             System.out.println("Invalid");
     }
-
-
 }
